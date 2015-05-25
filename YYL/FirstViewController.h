8c96fda6ae0b4c41848e21d812986d667ca072c7
admin <admin@macbook-1deMacBook-Air.local>
2015-05-25 17:21:48 +0800

    Initial Commit

---
+++ YYL/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  YYL
+//
+//  Created by admin on 15/5/25.
+//  Copyright (c) 2015年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ YYL/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  YYL
+//
+//  Created by admin on 15/5/25.
+//  Copyright (c) 2015年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
